f806f5be4b0180590239cd046c20987caa2723e0
mac <214180063@qq.com>
2019-07-01 13:56:24 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-ios_pod_library_test_Example/Pods-ios_pod_library_test_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_pod_library_test_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ios_pod_library_test_Example
+@end
---
+++ Example/Pods/Target Support Files/ios_pod_library_test/ios_pod_library_test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ios_pod_library_test : NSObject
+@end
+@implementation PodsDummy_ios_pod_library_test
+@end
